03a2a117cbfdef27114a02b149e70b82d70b125d
Jonas Lomholdt <jonaslomholdt@gmail.com>
2015-08-26 04:18:51 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+new Vue({
+  el: '#app',
+
+  data: {
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    days: 0,
+    diff: 0,
+    returnDate: '2015-08-27 02:23',
+  },
+
+  methods: {
+    setDiff: function(){
+      var now = moment();
+      var then = moment(this.returnDate);
+      this.diff = then.diff(now);
+    },
+    setTimeFromMillis: function(){
+      this.seconds = parseInt((this.diff / 1000) % 60);
+      this.minutes = parseInt((this.diff / (1000*60)) % 60);
+      this.hours = parseInt((this.diff / (1000*60*60)) % 24);
+      this.days = parseInt((this.diff / (1000*60*60*24)) % 7);
+    },
+    updateTime: function(){
+      this.setDiff();
+      this.setTimeFromMillis();
+    }
+  },
+
+  ready: function(){
+    moment.locale('dk');
+    this.updateTime();
+    setInterval(this.updateTime, 1000);
+  }
+});
+
+$('document').ready(function() {
+                $('#app').tubular({
+                  videoId: 'A9Tp5fl18Ho',
+                  mute: false,
+                  start: 491
+                });
+        });
